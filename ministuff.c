bf5821f3e0ea4542be16eb9ce1dac925d891416c
dpaes-so <diogo.soeiroavila@gmail.com>
2025-03-09 16:28:14 +0000

    Add files via upload

--- ministuff.c
+++ ministuff.c
@@ -1,9 +1,11 @@
 #define _GNU_SOURCE
 #define READLINE_LIBRARY
+
+#include <stdio.h>  // Include this before readline
+#include <string.h>
+#include <stdlib.h>
 #include <readline/readline.h>
 #include <readline/history.h>
-#include <stdio.h>
-#include <string.h>
 
 extern void rl_replace_line(const char *text, int clear_undo);
 
@@ -12,16 +14,26 @@ int main()
     while (1)
     {
         char *input = readline("shell> ");
+        if (!input)  
+            break;
+
         if (!strcmp(input, "hclear"))
             clear_history();
-        if (input && *input)
+        if(!strcmp(input,"cd"))
+        {
+            rl_replace_line("not yet xD", 0); //not working idk im angy >:(
+            rl_redisplay();
+        }
+        if(!strcmp(input,"$HOME"))
+            printf("Home directory: %s\n", getenv("HOME"));
+        if (*input)
         {
             printf("HERE  \n");
             add_history(input);
         }
-
-        // Replace the input line in the readline buffer
-        rl_replace_line("new line", 0);
-        rl_redisplay();  // Needed to update the displayed input line
+        printf("INPUT = %s\n",input);
+        free(input);
     }
+
+    return 0;
 }
